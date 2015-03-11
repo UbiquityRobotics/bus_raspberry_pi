2a262646f4e4ee6c2c28577b81d6b84339b4466b
Wayne C. Gramlich <Wayne@Gramlich.Net>
2015-03-11 14:57:15 -0700

    Made member of SetPointInfo start with an underscore.

--- bus_raspberry_pi.ino
+++ bus_raspberry_pi.ino
@@ -2,9 +2,9 @@
 //
 // This code drives the bus_beagle_bone_black board.
 
-#include "Bus_Slave.h"
-#include "Frame_Buffer.h"
-#include "bus_server.h"
+#include <Bus_Slave.h>
+#include <Frame_Buffer.h>
+#include <bus_server.h>
 
 #define TEST TEST_BUS_LINE
 
